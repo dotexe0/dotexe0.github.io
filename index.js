7b3e7ad0ac935aec7dbd20c83e4e0ba7a1a16926
Daniel Varela <dotexe0@gmail.com>
2017-04-03 23:18:33 -0700

    webpack updated

---
+++ index.js
@@ -0,0 +1,4 @@
+$(document).ready((e) => {
+e.preventDefault();
+console.log('hello world.')
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,47 @@
-var path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+const config = {
   entry: './index.js',
   output: {
+    // must be absolute path, that's why we use path module
+    // be sure that work on any os
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    // resolve problem about the img with a var name
+    publicPath: 'build/'
   },
-  presets: ['es2015', 'stage-0']
+  module: {
+    rules: [
+      {
+        use: 'babel-loader', // don't forget .babelrc
+        test: /\.js$/
+      },
+      {
+        // need to be in this order cause this is from
+        // the right to the left
+        // this make the css inline in the head
+        // use: ['style-loader', 'css-loader'],
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
+        test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader' // first one append
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
+
+module.exports = config;
