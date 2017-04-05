be488cff3ab2b3ffbb749edd4a14830dcfbe98b1
Daniel Varela <dotexe0@gmail.com>
2017-04-04 22:32:11 -0700

    placed css in css folder and added navbar

--- index.js
+++ index.js
@@ -2,4 +2,37 @@
 $(document).ready((e) => {
 // e.preventDefault();
 console.log('hello world.')
+
+
+// sticky nav-bar
+  const  mn = $(".main-nav");
+  const mns = "main-nav-scrolled";
+  let hdr = $('header').height();
+
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > hdr ) {
+      mn.addClass(mns);
+    } else {
+      mn.removeClass(mns);
+    }
+  });
+
+// slow page scroll to href anchor tag
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
 });
